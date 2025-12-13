5165413d54e3b22c6801d09308095f9e25ac5be5
BastianFrey <ba.frey@duocuc.cl>
2025-12-13 03:17:57 -0300

    intento 2 de la implementacion

--- app/src/main/java/com/example/levelupgamer/data/model/Producto.kt
+++ app/src/main/java/com/example/levelupgamer/data/model/Producto.kt
@@ -6,14 +6,29 @@ import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "producto")
 data class Producto(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    @SerializedName("codigo")
     val codigo: String = "",
+
+    @SerializedName("nombre")
     val nombre: String = "",
+
+    @SerializedName("descripcion")
     val descripcion: String = "",
+
+    @SerializedName("precio")
     val precio: Double = 0.0,
+
+    @SerializedName("categoria")
     val categoria: String = "",
+
     val imagenRes: Int = 0,
+
     @SerializedName("imagenUrl")
     val imagenUrl: String? = null,
+
+    @SerializedName("activo")
     val activo: Boolean = true
 )
